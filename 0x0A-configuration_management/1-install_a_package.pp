95ea1dd6d90ac6314603f64c35ea86dcdd8bdfca
KiraiX77 <kiraix77@gmail.com>
2024-02-24 02:49:19 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Using Puppet, install flask from pip3.
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
